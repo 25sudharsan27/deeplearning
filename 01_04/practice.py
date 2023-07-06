d9e0dc7aed4243282d4e4d6fd54e63dd9c5ca778
Sudharsan B <117500265+25sudharsan27@users.noreply.github.com>
2023-07-06 18:58:35 +0530

    Add files via upload

---
+++ 01_04/practice.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+img=np.array([[[0,0,0],[0,0,0],[0,0,0]],
+              [[0,0,0],[0,0,0],[0,0,0]],
+              [[0,0,0],[0,0,0],[0,0,0]]])
+
+plt.axis("off")
+plt.imshow(img)
